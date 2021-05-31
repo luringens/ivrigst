e6b93a407d0fb97657812d26f71134f13e8eac36
Stian Soltvedt <stian@soltvedt.info>
2021-05-31 17:44:29 +0200

    Add model loading and basic shader

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,95 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    reflect::TypeUuid,
+    render::{
+        pipeline::{PipelineDescriptor, RenderPipeline},
+        render_graph::{base, AssetRenderResourcesNode, RenderGraph},
+        renderer::RenderResources,
+        shader::{ShaderStage, ShaderStages},
+    },
+};
+
+const MODEL_PATH: &'static str = "model.obj";
 
 fn main() {
-    App::build().add_system(hello_world.system()).run();
+    color_backtrace::install();
+    App::build()
+        .insert_resource(Msaa { samples: 4 })
+        .insert_resource(WindowDescriptor {
+            title: "Medvis".to_string(),
+            width: 1920.,
+            height: 1080.,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(bevy_obj::ObjPlugin)
+        .add_plugin(bevy_stl::StlPlugin)
+        .add_plugin(bevy_orbit_controls::OrbitCameraPlugin)
+        .add_asset::<MyMaterial>()
+        .add_startup_system(setup.system())
+        .run();
 }
 
-fn hello_world() {
-    println!("Hello, world!");
+#[derive(RenderResources, Default, TypeUuid)]
+#[uuid = "0805ae06-bfbc-4e78-86bb-c1a4f143c6ad"]
+struct MyMaterial {
+    pub color: Color,
+}
+
+const VERTEX_SHADER: &'static str = include_str!("./vertex.glsl");
+const FRAGMENT_SHADER: &'static str = include_str!("./fragment.glsl");
+
+fn setup(
+    mut commands: Commands,
+    mut pipelines: ResMut<Assets<PipelineDescriptor>>,
+    mut shaders: ResMut<Assets<Shader>>,
+    mut materials: ResMut<Assets<MyMaterial>>,
+    mut render_graph: ResMut<RenderGraph>,
+    asset_server: Res<AssetServer>,
+) {
+    // Create a new shader pipeline
+    let pipeline_handle = pipelines.add(PipelineDescriptor::default_config(ShaderStages {
+        vertex: shaders.add(Shader::from_glsl(ShaderStage::Vertex, VERTEX_SHADER)),
+        fragment: Some(shaders.add(Shader::from_glsl(ShaderStage::Fragment, FRAGMENT_SHADER))),
+    }));
+
+    // Add an AssetRenderResourcesNode to our Render Graph. This will bind MyMaterial resources to
+    // our shader
+    render_graph.add_system_node(
+        "my_material",
+        AssetRenderResourcesNode::<MyMaterial>::new(true),
+    );
+
+    // Add a Render Graph edge connecting our new "my_material" node to the main pass node. This
+    // ensures "my_material" runs before the main pass
+    render_graph
+        .add_node_edge("my_material", base::node::MAIN_PASS)
+        .unwrap();
+
+    // Create a new material
+    let material = materials.add(MyMaterial {
+        color: Color::rgb(0.6, 0.7, 0.9),
+    });
+
+    // Camera
+    commands
+        .spawn_bundle(PerspectiveCameraBundle {
+            transform: Transform::from_translation(Vec3::ONE).looking_at(Vec3::ZERO, Vec3::Y),
+            ..Default::default()
+        })
+        .insert(bevy_orbit_controls::OrbitCamera {
+            distance: 200.0,
+            ..Default::default()
+        });
+
+    commands
+        .spawn_bundle(MeshBundle {
+            mesh: asset_server.load(MODEL_PATH),
+            transform: Transform::from_translation(Vec3::new(0.0, -350.0, 50.0)),
+            render_pipelines: RenderPipelines::from_pipelines(vec![RenderPipeline::new(
+                pipeline_handle,
+            )]),
+            ..Default::default()
+        })
+        .insert(material);
 }
