c08325387b9127ad48f991fa0a8701995950e8c3
Stian Soltvedt <stian@soltvedt.info>
2021-10-27 13:32:59 +0200

    Update syn and quote in derive macro

--- render_gl_derive/src/lib.rs
+++ render_gl_derive/src/lib.rs
@@ -1,5 +1,7 @@
 #![recursion_limit = "128"]
 
+use proc_macro::TokenStream;
+
 extern crate proc_macro;
 extern crate syn;
 #[macro_use]
@@ -7,19 +9,17 @@ extern crate quote;
 
 #[proc_macro_derive(VertexAttribPointers, attributes(location))]
 pub fn vertex_attrib_pointers_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let s = input.to_string();
-    let ast = syn::parse_derive_input(&s).unwrap();
-    let gen = generate_impl(&ast);
-    gen.parse().unwrap()
+    let ast = syn::parse(input).unwrap();
+    generate_impl(&ast)
 }
 
-fn generate_impl(ast: &syn::DeriveInput) -> quote::Tokens {
+fn generate_impl(ast: &syn::DeriveInput) -> TokenStream {
     let ident = &ast.ident;
     let generics = &ast.generics;
     let where_clause = &ast.generics.where_clause;
-    let fields_vertex_attrib_pointer = generate_vertex_attrib_pointer_calls(&ast.body);
+    let fields_vertex_attrib_pointer = generate_vertex_attrib_pointer_calls(&ast.data);
 
-    quote! {
+    let gen = quote! {
         impl #ident #generics #where_clause {
             #[allow(unused_variables)]
             pub fn vertex_attrib_pointers() {
@@ -29,26 +29,33 @@ fn generate_impl(ast: &syn::DeriveInput) -> quote::Tokens {
                 #(#fields_vertex_attrib_pointer)*
             }
         }
-    }
+    };
+
+    gen.into()
 }
 
-fn generate_vertex_attrib_pointer_calls(body: &syn::Body) -> Vec<quote::Tokens> {
+fn generate_vertex_attrib_pointer_calls(body: &syn::Data) -> Vec<Box<dyn quote::ToTokens>> {
     match body {
-        &syn::Body::Enum(_) => panic!("VertexAttribPointers can not be implemented for enums"),
-        &syn::Body::Struct(syn::VariantData::Unit) => {
-            panic!("VertexAttribPointers can not be implemented for Unit structs")
-        }
-        &syn::Body::Struct(syn::VariantData::Tuple(_)) => {
-            panic!("VertexAttribPointers can not be implemented for Tuple structs")
-        }
-        &syn::Body::Struct(syn::VariantData::Struct(ref s)) => s
+        &syn::Data::Union(_) => panic!("VertexAttribPointers can not be implemented for unions"),
+        &syn::Data::Enum(_) => panic!("VertexAttribPointers can not be implemented for enums"),
+        // &syn::Data::Struct(syn::VariantData::Unit) => {
+        //     panic!("VertexAttribPointers can not be implemented for Unit structs")
+        // }
+        // &syn::Data::Struct(syn::VariantData::Tuple(_)) => {
+        //     panic!("VertexAttribPointers can not be implemented for Tuple structs")
+        // }
+        // &syn::Data::Struct(syn::VariantData::Struct(ref s)) => s
+        &syn::Data::Struct(ref data_struct) => data_struct
+            .fields
             .iter()
             .map(generate_struct_field_vertex_attrib_pointer_call)
             .collect(),
     }
 }
 
-fn generate_struct_field_vertex_attrib_pointer_call(field: &syn::Field) -> quote::Tokens {
+fn generate_struct_field_vertex_attrib_pointer_call(
+    field: &syn::Field,
+) -> Box<dyn quote::ToTokens> {
     let field_name = match field.ident {
         Some(ref i) => format!("{}", i),
         None => String::from(""),
@@ -56,12 +63,20 @@ fn generate_struct_field_vertex_attrib_pointer_call(field: &syn::Field) -> quote
     let location_attr = field
         .attrs
         .iter()
-        .filter(|a| a.value.name() == "location")
+        .filter_map(|attr| attr.parse_meta().ok())
+        .filter(|meta| match meta {
+            syn::Meta::NameValue(syn::MetaNameValue { ref path, .. }) => path
+                .get_ident()
+                .map(|ident| ident == "location")
+                .unwrap_or(false),
+            _ => false,
+        })
+        .take(1)
         .next()
         .unwrap_or_else(|| panic!("Field {} is missing #[location = ?] attribute", field_name));
 
-    let location_value = match location_attr.value {
-        syn::MetaItem::NameValue(_, ref literal @ syn::Lit::Int(_, _)) => literal,
+    let location_value = match location_attr {
+        syn::Meta::NameValue(syn::MetaNameValue { ref lit, .. }) => lit,
         _ => panic!(
             "Field {} location attribute value must be an integer literal",
             field_name
@@ -69,11 +84,13 @@ fn generate_struct_field_vertex_attrib_pointer_call(field: &syn::Field) -> quote
     };
 
     let field_ty = &field.ty;
-    quote! {
+    let gen = quote! {
         let location = #location_value;
         unsafe {
             #field_ty::vertex_attrib_pointer(stride, location, offset);
         }
         let offset = offset + ::std::mem::size_of::<#field_ty>();
-    }
+    };
+
+    Box::new(gen)
 }
